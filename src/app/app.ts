83d3441ca30f063a17518f9789f4408a13d80405
Denis Espanhol <denisgfespanhol@gmail.com>
2026-03-18 09:16:07 -0300

    Inserting grid layout for 12 columns

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Homepage } from './homepage/homepage.component';
+import { Navbar } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet, Homepage ],
+  imports: [ RouterOutlet, Homepage, Navbar ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
