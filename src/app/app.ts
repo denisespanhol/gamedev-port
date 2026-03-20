f394bd7ae3df427ee94fdbc015c23105194cd22e
Denis Espanhol <denisgfespanhol@gmail.com>
2026-03-20 11:33:13 -0300

    finishing about section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Homepage } from './homepage/homepage.component';
 import { Navbar } from './navbar/navbar.component';
+import { ProfessionalGames } from "./professional-games/professional-games.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet, Homepage, Navbar ],
+  imports: [RouterOutlet, Homepage, Navbar, ProfessionalGames],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
