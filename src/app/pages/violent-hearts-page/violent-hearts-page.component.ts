ac9f7c960289207f04f078112fa1bb67e5d5ed7a
Denis Espanhol <denisgfespanhol@gmail.com>
2026-03-25 12:09:30 -0300

    Creating Violent Hearts Page

---
+++ src/app/pages/violent-hearts-page/violent-hearts-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-violent-hearts-page',
+  imports: [],
+  templateUrl: './violent-hearts-page.component.html',
+  styleUrl: './violent-hearts-page.component.scss',
+})
+export class ViolentHeartsPage {}
