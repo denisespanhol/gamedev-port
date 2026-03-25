ef5bb429899a40e40f90a528126a31362181450d
Denis Espanhol <denisgfespanhol@gmail.com>
2026-03-25 12:03:55 -0300

    Finishing Ponge Defense Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainContent } from './main-content/main-content.component';
 import { ShardSquadPage } from './pages/shard-squad-page/shard-squad-page.component';
 import { SpookyWorkersPage } from './pages/spooky-workers-page/spooky-workers-page.component';
+import { PongDefensePage } from './pages/pong-defense-page/pong-defense-page.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: 'spooky-workers',
         component: SpookyWorkersPage
+    },
+    {
+        path: 'pong-defense',
+        component: PongDefensePage
     }
 ];
